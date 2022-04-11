5b97b83c6efe239732d3006fe1e3c064c07639de
yhawamdeh <yalhawamdeh@gmail.com>
2022-04-11 07:01:02 -0400

    updates

---
+++ app.js
@@ -0,0 +1,10 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++){
+        console.log(profileDataArr[i]);
+    }
+};
+
+printProfileData(profileDataArgs);
