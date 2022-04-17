f3633a8a49e9217045bbde99985b810005117ece
yhawamdeh <yalhawamdeh@gmail.com>
2022-04-17 17:19:36 -0400

    updates

--- app.js
+++ app.js
@@ -129,5 +129,9 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+          console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
